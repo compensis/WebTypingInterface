855c5da314de1ceb61e6f0360fedea2ef966408c
Philipp Compensis <github@0xg.de>
2026-09-09 15:20:13 +0200

    Use Boot Protocol for USB HID keyboard

--- lib/USBBootKeyboard/src/USBHID.cpp
+++ lib/USBBootKeyboard/src/USBHID.cpp
@@ -208,7 +208,7 @@ extern "C" uint16_t tusb_hid_load_descriptor(uint8_t * dst, uint8_t * itf)
     uint8_t descriptor[TUD_HID_INOUT_DESC_LEN] = {
         // HID Input & Output descriptor
         // Interface number, string index, protocol, report descriptor len, EP OUT & IN address, size & polling interval
-        TUD_HID_INOUT_DESCRIPTOR(*itf, str_index, HID_ITF_PROTOCOL_NONE, tinyusb_hid_device_descriptor_len, ep_out, (uint8_t)(0x80 | ep_in), 64, 1)
+        TUD_HID_INOUT_DESCRIPTOR(*itf, str_index, HID_ITF_PROTOCOL_KEYBOARD, tinyusb_hid_device_descriptor_len, ep_out, (uint8_t)(0x80 | ep_in), 64, 1)
     };
     *itf+=1;
     memcpy(dst, descriptor, TUD_HID_INOUT_DESC_LEN);
